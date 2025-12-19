c106f2f5f923c38193ae9139346f834200fabe56
rohit marmat <rohitmarmat4922@gmail.com>
2025-12-19 07:27:28 -0800

    Import axios and configure base URL
    Added axios and set global base URL for API calls.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-
+import axios from 'axios'
+// Set the global base URL for all API calls
+axios.defaults.baseURL = 'https://flipr-project-9qdg.onrender.com';
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
